3cbb149ff68f94dd9f86c9eb6aaf370ef008f1e4
obadominion <henrywebdevs@gmail.com>
2021-08-26 10:48:53 -0700

    The first c file in this project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main
+{
+//This program initialises the string for printing
+char Mystr [] = "Programming is like building a multilingual puzzle \n";
+puts(Mystr); //Printing the string to stdout
+Return 0;
+}
