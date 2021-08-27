c871b00645a0df5b5e4ab0ef413899ddd522cbad
obadominion <henrywebdevs@gmail.com>
2021-08-27 08:39:08 -0700

    print small alphabet letters first and then the capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+  
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char j = 'a';
+	char k = 'A';
+
+	while (j <= 'z')
+	{
+		putchar(j);
+		j++;
+	}
+	while (k <= 'Z')
+	{
+		putchar(k);
+		k++;
+	}
+	putchar('\n');
+	return (0);
+
+}
