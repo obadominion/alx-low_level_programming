1ba6c7519a1ee6b2485db0b2952a65b826762838
obadominion <henrywebdevs@gmail.com>
2021-09-01 04:06:42 -0700

    print the characterts put char

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include ".main.h"
+/**
+* main - function prints the text holberton"
+*
+* Return: return 0
+*/
+int main(void)
+{
+char alx[9] = "_putchar";
+int ch;
+for (ch = 0; ch <= 7; ch++)
+{
+_putchar(alx[ch]);
+}
+_putchar('\n');
+return (0);
+}
