6afddbe09d226a2b679b1ec1bd9a7e2c76b9793d
obadominion <henrywebdevs@gmail.com>
2021-09-02 07:15:14 -0700

    print ou the uppercse

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_isupper - function that checks for uppercase characters
+*Its a c program
+*Return: 1 if uppercase, else 0
+*/
+int main
+{
+int _isupper(int c)
+{
+if (c>='A' && c<='Z')
+return 1;
+else
+{
+return 0;
+}
