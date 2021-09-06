aaeed9c874186299ee347ab27c2ed1656521f6e9
obadominion <henrywebdevs@gmail.com>
2021-09-05 20:48:05 -0700

    reassign a variable using a pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,17 @@
-/*this program resets a memory location
-* to 98
-* resets the value n to 98
-*void reset_to_98(int *n) - we use this function
-* return 0
-*/
-void reset_to_98(int *n){
-int i = 98;
-int *n;
-n = &i; /* store address of i in pointer variable*/
-printf("Address of i variable: %x\n", &n);
-/* address stored in pointer variable */
-printf("Address stored in n variable: %x\n", i);
-return 0;
+/**
+ * This program resets a memory location
+ * resets the value n to 98
+ * void reset_to_98(int *n) - we use this function
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = 402;
+    _putchar("n=%d\n", n);
+    reset_to_98(&n);
+    _putchar("n=%d\n", n);
+    return (0);
 }
