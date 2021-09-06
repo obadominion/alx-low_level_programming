6e505bf14f1a08a9ee63b2017069a17c445c700b
obadominion <henrywebdevs@gmail.com>
2021-09-06 08:14:38 -0700

    commit these changes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,13 @@
-/**
- * This program resets a memory location
- * resets the value n to 98
- * void reset_to_98(int *n) - we use this function
- * 
- * Return: Always 0.
- */
-int main(void)
-{
-    int n;
+#include "main.h"
 
-    n = 402;
-    _putchar("n=%d\n", n);
-    reset_to_98(&n);
-    _putchar("n=%d\n", n);
-    return (0);
+/**
+ * reset_to_98 - updates value of pointer *n to 98
+ * @n: pointer to be updated
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
