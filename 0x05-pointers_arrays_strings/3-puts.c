6c6d6f56072f519bb3570e37b762a6419d040dd7
obadominion <henrywebdevs@gmail.com>
2021-09-06 10:04:45 -0700

    put character

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,21 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
-* _puts - prints a string, str, with newline
-* @str: string to be printed
-*
-* Return: void
-*/
-
+ *_puts - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * On success: returns no error
+ */
 void _puts(char *str)
 {
-int count = 0;
+	int i = 0;
 
-while (*(str + count) != '\0')
-{
-if (*(str + count) != '"')
-_putchar(*(str + count));
-count++;
-}
-_putchar('\n');
+	while(*(str + i) != '\0')
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
 }
