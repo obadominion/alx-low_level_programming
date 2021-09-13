f06e4f47c5d23324bfa52ef1e4b95c45c8922a46
obadominion <henrywebdevs@gmail.com>
2021-09-13 11:30:13 -0700

    adding the string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _memcpy - copies memory area
- * @dest: destination of copy
- * @src: memory area to copy
- * @n: number of bytes to copy
- *
- * Return: pointer to dest
- */
+* _memcpy - copies memory area
+* @dest: destination of copy
+* @src: memory area to copy
+* @n: number of bytes to copy
+*
+* Return: pointer to dest
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
