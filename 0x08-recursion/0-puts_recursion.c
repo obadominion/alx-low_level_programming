688409f1bafcfcde82fc61eb4236dce736e8fdc6
obadominion <henrywebdevs@gmail.com>
2021-09-15 11:15:08 -0700

    do it as i like

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,3 @@
-  
 #include "main.h"
 
 /**
@@ -8,11 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
